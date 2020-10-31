b387c6a0da1f4cc78dfdaad14d0e46394dfb43cc
Dalton Morrel <daltonmorrel7&@gmail.com>
2020-10-31 13:40:32 -0400

    generate password function()

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,50 @@
 // Assignment code here
 
 
+var chooseChar = function() 
+
+{
+  // uppercase characters
+  var upperChar = confirm('Would you like to include uppercase characters?');
+
+    if (!upperChar) 
+    {
+      upperChar = "";
+    } else 
+    {
+      upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+
+
+};
+
+var generatePassword = function() 
+{
+  var lengthPass = prompt('How many characters would you like your password to contain? Please enter a number between "8" and "128".');
+  lengthPass = parseInt(lengthPass);
+  console.log('Password length is ' + lengthPass);
+
+  if (lengthPass < 8 || lengthPass > 128) 
+  {
+    return generatePassword();
+  }
+
+  // creates password on chosen input
+  var selectPassword = chooseChar();
+  var returnPassword = "";
+
+  for (var i = 0; i < lengthPass; i++) 
+  {
+    returnPassword = selectPassword[Math.floor(Math.random() * selectPassword.length)];
+  }
+  console.log("Password is " + returnPassword);
+
+  return returnPassword;
+};
+
+// generate password function
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
